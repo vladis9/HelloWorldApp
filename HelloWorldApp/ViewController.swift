89e956dfe1aa86128b90ebcd7a98a9d038a08fc4
Vlad Zinevich <vladisznv@gmail.com>
2021-05-03 09:50:03 +0300

    add button functions

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            startButton.setTitle("Show Hello", for: .normal)
+        }
+        
+    }
+    
 }
 
